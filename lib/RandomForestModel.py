6739a6ee8a0b07b1b981a4eaf53ad6847f870626
hamza <hanaechouali@gmail.com>
2022-01-02 23:24:05 +0100

    Updating projects

--- lib/RandomForestModel.py
+++ lib/RandomForestModel.py
@@ -1,3 +1,27 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5c28ce43c17c328399f9def457e4317aee104976bbdd67fef5d69e50b7f71bca
-size 970
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+
+
+class LogisticRegressionModel:
+    def __init__(self, x_train, x_test, y_train, y_test, parameters):
+        self.x_train = x_train
+        self.x_test = x_test
+        self.y_train = y_train
+        self.y_test = y_test
+
+        self.parameters = parameters
+        self.clf = None
+
+    def train(self, scaler, cv=3):
+        pipe = Pipeline([('scaler', scaler), ('rf', RandomForestClassifier())])
+
+        self.clf = GridSearchCV(pipe, self.parameters, cv=cv)
+        self.clf.fit(self.x_train, self.y_train)
+
+    def test(self):
+        print('\nModel: Random Forest')
+        print('Returned hyperparameter: {}'.format(self.clf.best_params_))
+        print('Best accuracy in train is: {}'.format(self.clf.best_score_))
+        print('Classification accuracy on test is: {}'.format(self.clf.score(self.x_test, self.y_test)))
--- lib/SVMModel.py
+++ lib/SVMModel.py
@@ -1,3 +1,26 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ec748efe8aaa73363aa2f9d9aa524cb879a092a176e3c748380e13d7a1a7b688
-size 980
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import LinearSVC
+
+
+class LogisticRegressionModel:
+    def __init__(self, x_train, x_test, y_train, y_test, parameters):
+        self.x_train = x_train
+        self.x_test = x_test
+        self.y_train = y_train
+        self.y_test = y_test
+
+        self.parameters = parameters
+        self.clf = None
+
+    def train(self, scaler, max_iter=3000, cv=3):
+        pipe = Pipeline([('scaler', scaler), ('svc', LinearSVC(max_iter=max_iter))])
+
+        self.clf = GridSearchCV(pipe, self.parameters, cv=cv)
+        self.clf.fit(self.x_train, self.y_train)
+
+    def test(self):
+        print('\nModel: Support Vector Machine')
+        print('Returned hyperparameter: {}'.format(self.clf.best_params_))
+        print('Best accuracy in train is: {}'.format(self.clf.best_score_))
+        print('Classification accuracy on test is: {}'.format(self.clf.score(self.x_test, self.y_test)))
