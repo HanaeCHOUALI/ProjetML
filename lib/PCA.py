ce5092add1e3b11a7d2d49c96a0b4351145a89ad
hamza <hanaechouali@gmail.com>
2022-01-09 19:48:29 +0100

    Final code v2

--- lib/PCA.py
+++ lib/PCA.py
@@ -1,26 +1,23 @@
 import numpy as np
-from sklearn.decomposition import KernelPCA
 
 
 class PrincipalComponentAnalysis:
-    def __init__(self, n_components, x_train, x_test):
-        self.n_components = n_components
+    def __init__(self, model, x_train, x_test, kernel=False):
         self.x_train = x_train
         self.x_test = x_test
 
-        self.pca = None
+        self.model = model
+        self.kernel = kernel
 
     def fit(self, scaler):
         self.x_train = scaler.fit_transform(self.x_train)
-        self.pca = KernelPCA(n_components=self.n_components, kernel='poly')
 
-        self.pca.fit(self.x_train)
-        self.x_train = self.pca.transform(self.x_train)
-        self.x_test = self.pca.transform(self.x_test)
+        self.model.fit(self.x_train)
+        self.x_train = self.model.transform(self.x_train)
+        self.x_test = self.model.transform(self.x_test)
 
     def test(self):
-        print('Explained variance ratio ', self.pca.explained_variance_ratio_[:300])
-
-        var = np.cumsum(self.pca.explained_variance_ratio_[:300])
-        print('Cumulative explained variance ratio ', var)
-        pass
+        if not self.kernel:
+            print('Explained variance ratio ', self.model.explained_variance_ratio_)
+            var = np.cumsum(self.model.explained_variance_ratio_)
+            print('Cumulative explained variance ratio ', var)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import time
 import numpy as np
 import pandas as pd
 import torch
+from sklearn.decomposition import KernelPCA, PCA
 from torch import optim
 from torch.utils.data import DataLoader, TensorDataset
 from sklearn.model_selection import train_test_split
@@ -13,9 +15,11 @@ from lib.RandomForestModel import RandomForestModel
 from lib.SVMModel import SVMModel
 
 if __name__ == '__main__':
+    # Read data
     data = pd.read_csv('data/data.csv', sep=",", index_col=0)
     labels = pd.read_csv('data/labels.csv', sep=",", index_col=0)
 
+    # Replace labels names with numerical values
     labels_unique_values = {
         'PRAD': 1,
         'LUAD': 2,
@@ -26,6 +30,7 @@ if __name__ == '__main__':
 
     labels['Class'] = labels['Class'].map(labels_unique_values)
 
+    # Create training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(
         data,
         labels,
@@ -33,16 +38,27 @@ if __name__ == '__main__':
         random_state=42
     )
 
-    # PCA - 500 components
-    pca = PrincipalComponentAnalysis(500, X_train, X_test)
+    # Build PCA model considering only the 3 first components
+    model = PCA(n_components=3)
+    pca = PrincipalComponentAnalysis(model, X_train, X_test, kernel=False)
     pca.fit(scaler=StandardScaler())
-    pca.test()
+
+    # Apply the dimension reduction to the training and testing sets
     x_train_pca = pca.x_train
     x_test_pca = pca.x_test
 
-    # Build Random Forest model
+    # Build Kernel PCA model considering only the 3 first components and polynomial kernel
+    model = KernelPCA(n_components=3, kernel='poly')
+    pca_kernel = PrincipalComponentAnalysis(model, X_train, X_test, kernel=True)
+    pca_kernel.fit(scaler=StandardScaler())
+
+    # Apply the dimension reduction to the training and testing sets
+    x_train_pca_kernel = pca_kernel.x_train
+    x_test_pca_kernel = pca_kernel.x_test
+
+    # Build Random Forest model without PCA
     rf_parameters = {'rf__max_depth': np.arange(1, 101, 5)}
-    print('\nModel: Random Forest')
+    print('\nModel: Random Forest without PCA')
     rf_model = RandomForestModel(
         X_train,
         X_test,
@@ -50,12 +66,14 @@ if __name__ == '__main__':
         np.array(np.transpose(y_test)).reshape(-1),
         rf_parameters
     )
+    init_time = time.time()
     rf_model.train(scaler=StandardScaler(), cv=3)
+    training_time = time.time() - init_time
     rf_model.test()
+    print('Training time:', training_time)
 
-    # Build Random Forest model - PCA
-    rf_parameters = {'rf__max_depth': np.arange(1, 101, 5)}
-    print('\nModel: Random Forest - PCA with n_components = 100')
+    # Build Random Forest model with PCA
+    print('\nModel: Random Forest with PCA')
     rf_model_pca = RandomForestModel(
         x_train_pca,
         x_test_pca,
@@ -63,12 +81,30 @@ if __name__ == '__main__':
         np.array(np.transpose(y_test)).reshape(-1),
         rf_parameters
     )
+    init_time = time.time()
     rf_model_pca.train(scaler=StandardScaler(), cv=3)
+    training_time = time.time() - init_time
     rf_model_pca.test()
+    print('Training time:', training_time)
 
-    # Build Logistic Regression model
+    # Build Random Forest model with Kernel PCA
+    print('\nModel: Random Forest with Kernel PCA')
+    rf_model_pca_kernel = RandomForestModel(
+        x_train_pca_kernel,
+        x_test_pca_kernel,
+        np.array(np.transpose(y_train)).reshape(-1),
+        np.array(np.transpose(y_test)).reshape(-1),
+        rf_parameters
+    )
+    init_time = time.time()
+    rf_model_pca_kernel.train(scaler=StandardScaler(), cv=3)
+    training_time = time.time() - init_time
+    rf_model_pca_kernel.test()
+    print('Training time:', training_time)
+
+    # Build Logistic Regression model without PCA
     lr_parameters = {'logreg__C': np.logspace(-2, 2, 5, base=2)}
-    print('\nModel: Logistic Regression')
+    print('\nModel: Logistic Regression without PCA')
     lr_model = LogisticRegressionModel(
         X_train,
         X_test,
@@ -76,12 +112,45 @@ if __name__ == '__main__':
         np.array(np.transpose(y_test)).reshape(-1),
         lr_parameters
     )
+    init_time = time.time()
     lr_model.train(StandardScaler(), max_iter=3000, cv=3)
+    training_time = time.time() - init_time
     lr_model.test()
+    print('Training time:', training_time)
 
-    # Build SVM model
+    # Build Logistic Regression model with PCA
+    print('\nModel: Logistic Regression with PCA')
+    lr_model_pca = LogisticRegressionModel(
+        x_train_pca,
+        x_test_pca,
+        np.array(np.transpose(y_train)).reshape(-1),
+        np.array(np.transpose(y_test)).reshape(-1),
+        lr_parameters
+    )
+    init_time = time.time()
+    lr_model_pca.train(StandardScaler(), max_iter=5000, cv=3)
+    training_time = time.time() - init_time
+    lr_model_pca.test()
+    print('Training time:', training_time)
+
+    # Build Logistic Regression model with Kernel PCA
+    print('\nModel: Logistic Regression with Kernel PCA')
+    lr_model_pca_kernel = LogisticRegressionModel(
+        x_train_pca_kernel,
+        x_test_pca_kernel,
+        np.array(np.transpose(y_train)).reshape(-1),
+        np.array(np.transpose(y_test)).reshape(-1),
+        lr_parameters
+    )
+    init_time = time.time()
+    lr_model_pca_kernel.train(StandardScaler(), max_iter=5000, cv=3)
+    training_time = time.time() - init_time
+    lr_model_pca_kernel.test()
+    print('Training time:', training_time)
+
+    # Build SVM model without PCA
     svm_parameters = {'svc__C': np.logspace(-2, 2, 5, base=2)}
-    print('\nModel: Support Vector Machine')
+    print('\nModel: Support Vector Machine without PCA')
     svm_model = SVMModel(
         X_train,
         X_test,
@@ -89,8 +158,41 @@ if __name__ == '__main__':
         np.array(np.transpose(y_test)).reshape(-1),
         svm_parameters
     )
+    init_time = time.time()
     svm_model.train(StandardScaler(), max_iter=3000, cv=3)
+    training_time = time.time() - init_time
     svm_model.test()
+    print('Training time:', training_time)
+
+    # Build SVM model with PCA
+    print('\nModel: Support Vector Machine with PCA')
+    svm_model_pca = SVMModel(
+        x_train_pca,
+        x_test_pca,
+        np.array(np.transpose(y_train)).reshape(-1),
+        np.array(np.transpose(y_test)).reshape(-1),
+        svm_parameters
+    )
+    init_time = time.time()
+    svm_model_pca.train(StandardScaler(), max_iter=5000, cv=3)
+    training_time = time.time() - init_time
+    svm_model_pca.test()
+    print('Training time:', training_time)
+
+    # Build SVM model with Kernel PCA
+    print('\nModel: Support Vector Machine with Kernel PCA')
+    svm_model_pca_kernel = SVMModel(
+        x_train_pca_kernel,
+        x_test_pca_kernel,
+        np.array(np.transpose(y_train)).reshape(-1),
+        np.array(np.transpose(y_test)).reshape(-1),
+        svm_parameters
+    )
+    init_time = time.time()
+    svm_model_pca_kernel.train(StandardScaler(), max_iter=5000, cv=3)
+    training_time = time.time() - init_time
+    svm_model_pca_kernel.test()
+    print('Training time:', training_time)
 
     # Neural network
     train_tensor = TensorDataset(
